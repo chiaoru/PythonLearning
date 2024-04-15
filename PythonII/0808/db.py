845ea78a91f71405fbc34f05f94ea51a94f94976
Kate <chiaoru.wang@gmail.com>
2024-04-15 20:13:52 +0800

    Update db.py

--- PythonII/0808/db.py
+++ PythonII/0808/db.py
@@ -7,7 +7,7 @@ Created on Tue Aug  8 18:45:40 2023
 
 import mysql.connector
 
-connect = mysql.connector.connect(host='127.0.0.1', user='root', passwd='Chiao0923', database='myweb', auth_plugin='mysql_native_password', buffered=True)
+connect = mysql.connector.connect(host='127.0.0.1', user='root', passwd='', database='myweb', auth_plugin='', buffered=True)
 
 # 資料集 dataset
 cursor = connect.cursor()
