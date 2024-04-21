7f9d77d2ed338a1bdd69f5877f8073294abfe5e3
Kate <chiaoru.wang@gmail.com>
2024-04-21 16:25:50 +0800

    Delete PythonIII/lcc directory

--- PythonIII/lcc/product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Goods
-
-class GoodsAdmin(admin.ModelAdmin): # 希望在後台有自訂顯示的方式
-    list_display = ('name', 'price', 'c_date') # 在後台顯示的欄位名稱
-admin.site.register(Goods, GoodsAdmin)
